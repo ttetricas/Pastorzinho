399c9377824a74d6ce3ac9fb5d98e954dd574c45
ttetricas <estermeireles89@gmail.com>
2025-03-11 17:18:26 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// Dó, ré, mi, fá, fá, fá,
+#pragma warning disable CA1416
+Console.Write("Dó, "); 
+Console.Beep(1320, 200);
+Console.Write("ré, "); 
+Console.Beep(1485, 200);
+Console.Write("mi, "); 
+Console.Beep(1650, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(700);
+Console.Write("fá, "); 
+Console.Beep(1759, 200);
+Console.Write("fá,"); 
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, ré, dó, ré, ré, ré,
+Console.Write("Dó, "); 
+Console.Beep(1320, 200);
+Console.Write("ré, "); 
+Console.Beep(1485, 200);
+Console.Write("dó, "); 
+Console.Beep(1320, 200);
+Console.Write("ré, "); 
+Console.Beep(1485, 200);
+Thread.Sleep(700);
+Console.Write("ré, "); 
+Console.Beep(1485, 200);
+Console.Write("ré,"); 
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, sol, fá, mi, mi, mi,
+Console.Write("Dó, "); 
+Console.Beep(1320, 200);
+Console.Write("sol, "); 
+Console.Beep(1980, 200);
+Console.Write("fá, "); 
+Console.Beep(1759, 200);
+Console.Write("mi, "); 
+Console.Beep(1650, 200);
+Thread.Sleep(700);
+Console.Write("mi, "); 
+Console.Beep(1650, 200);
+Console.Write("mi,"); 
+Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+// Dó, ré, mi, fá, fá, fá.
+Console.Write("Dó, "); 
+Console.Beep(1320, 200);
+Console.Write("ré, "); 
+Console.Beep(1485, 200);
+Console.Write("mi, "); 
+Console.Beep(1650, 200);
+Console.Write("fá, "); 
+Console.Beep(1759, 200);
+Thread.Sleep(700);
+Console.Write("fá, "); 
+Console.Beep(1759, 200);
+Console.Write("fá."); 
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+#pragma warning restore CA1416
